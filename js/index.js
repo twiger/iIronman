16d4ff4936e2b7888d36661665aea62d219d90a4
twiger <promisemyself1215@gmai.com>
2015-08-12 14:21:26 +0800

    new page & change page mechanism

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(document)
     .ready(function() {
+        init_master();
+        $.ajaxSetup({ cache: false });
 
-        $("#master").load("master.html", function(responseTxt, statusTxt, xhr) {
+        $("#main_content").load("home.html", function(responseTxt, statusTxt, xhr) {
             init_master();
 
         });
@@ -9,6 +11,34 @@ $(document)
 
         });
 
+        $('.about.item').click(function() {
+            $("#main_content").load("competition_news.html", function(responseTxt, statusTxt, xhr) {
+                $("#master_menu > .item").removeClass("active");
+                $('.about.item').addClass("active");
+                $('#info_content').css("display","none");
+                $('#info_content').fadeIn(1000);
+            });
+        });
+
+        $('.home.item').click(function() {
+            $("#main_content").load("home.html", function(responseTxt, statusTxt, xhr) {
+                init_master();
+
+            });
+        });
+
+        $('.enrollment.item').click(function() {
+            $("#main_content").load("enrollment.html", function(responseTxt, statusTxt, xhr) {
+                $("#master_menu > .item").removeClass("active");
+                $('.enrollment.item').addClass("active");
+                $('#info_content').css("display","none");
+                $('#info_content').fadeIn(1000);
+            });
+        });
+
+
+
+        /*
         var pos = 0;
         var scroll_bool = true;
         $(window).scroll(function() {
@@ -18,12 +48,13 @@ $(document)
                 disable_scroll();
                 $('html, body').animate({
                     scrollTop: $("#main_content").offset().top - 50
-                }, 1000,function(){
+                }, 1000, function() {
                     enable_scroll();
                 });
                 scroll_bool = false;
             }
         });
+        */
 
 
 
@@ -34,9 +65,16 @@ function init_master() {
     $("#owl-demo").owlCarousel({
 
         autoPlay: 3000, //Set AutoPlay to 3 seconds
-        items: 4,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3],
+        itemsCustom: [
+            [0, 4],
+            [400, 4],
+            [700, 4],
+            [1000, 4],
+            [1200, 4],
+            [1600, 4]
+        ],
+        //itemsDesktop: [1199, 3],
+        //itemsDesktopSmall: [979, 3],
         rewindSpeed: 0,
         pagination: false
 
@@ -46,10 +84,10 @@ function init_master() {
 
 
 function preventDefault(e) {
-  e = e || window.event;
-  if (e.preventDefault)
-      e.preventDefault();
-  e.returnValue = false;  
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
 }
 
 function keydown(e) {
@@ -62,20 +100,20 @@ function keydown(e) {
 }
 
 function wheel(e) {
-  preventDefault(e);
+    preventDefault(e);
 }
 
 function disable_scroll() {
-  if (window.addEventListener) {
-      window.addEventListener('DOMMouseScroll', wheel, false);
-  }
-  window.onmousewheel = document.onmousewheel = wheel;
-  document.onkeydown = keydown;
+    if (window.addEventListener) {
+        window.addEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = wheel;
+    document.onkeydown = keydown;
 }
 
 function enable_scroll() {
     if (window.removeEventListener) {
         window.removeEventListener('DOMMouseScroll', wheel, false);
     }
-    window.onmousewheel = document.onmousewheel = document.onkeydown = null;  
+    window.onmousewheel = document.onmousewheel = document.onkeydown = null;
 }
