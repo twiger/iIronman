3c28ed042e88f690de242452c6fb73abae7239f6
twiger <promisemyself1215@gmai.com>
2015-08-13 09:41:53 +0800

    laptop layout

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 $(document)
     .ready(function() {
         init_master();
-        $.ajaxSetup({ cache: false });
+        $.ajaxSetup({
+            cache: false
+        });
 
         $("#main_content").load("home.html", function(responseTxt, statusTxt, xhr) {
             init_master();
+            $("#master_menu > .item").removeClass("active");
+            $('.home.item').addClass("active");
+            $('#info_content').css("display", "none");
+            $('#info_content').fadeIn(1000);
 
         });
         $(document).ajaxStop(function() {
@@ -17,7 +23,7 @@ $(document)
                 init_master();
                 $("#master_menu > .item").removeClass("active");
                 $('.home.item').addClass("active");
-                $('#info_content').css("display","none");
+                $('#info_content').css("display", "none");
                 $('#info_content').fadeIn(1000);
 
             });
@@ -27,7 +33,7 @@ $(document)
             $("#main_content").load("competition_news.html", function(responseTxt, statusTxt, xhr) {
                 $("#master_menu > .item").removeClass("active");
                 $('.about.item').addClass("active");
-                $('#info_content').css("display","none");
+                $('#info_content').css("display", "none");
                 $('#info_content').fadeIn(1000);
             });
         });
@@ -36,7 +42,7 @@ $(document)
             $("#main_content").load("enrollment.html", function(responseTxt, statusTxt, xhr) {
                 $("#master_menu > .item").removeClass("active");
                 $('.enrollment.item').addClass("active");
-                $('#info_content').css("display","none");
+                $('#info_content').css("display", "none");
                 $('#info_content').fadeIn(1000);
             });
         });
